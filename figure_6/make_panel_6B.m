795f0ca56c115f9757e012c6e7d2655b4eb79433
neonine2 <jerry.wang95@yahoo.ca>
2021-09-02 11:11:29 -0400

    figure 6 code

---
+++ figure_6/make_panel_6B.m
@@ -0,0 +1,43 @@
+function [] = make_panel_6B(fnamelist)
+load(fnamelist(2),'rel_eff_mat','rtotlist','kdlist')
+rel_eff = rel_eff_mat;
+paramlist = combvec(rtotlist,kdlist)';
+
+%interpolation
+F = scatteredInterpolant(paramlist(:,1),paramlist(:,2),...
+                                            rel_eff);
+rtotlist = logspace(2,5,16);
+kdlist = logspace(-2,0.5,12);
+paramlist = combvec(rtotlist,kdlist)';
+rel_eff_FULL = F(paramlist(:,1),paramlist(:,2));
+
+load(fnamelist(1),'rel_eff_mat','rtotlist','kdlist')
+rel_eff = cat(1,rel_eff_FULL, rel_eff_mat);
+paramlist = cat(1,paramlist, combvec(rtotlist,kdlist)');
+F = scatteredInterpolant(paramlist(:,1),paramlist(:,2),...
+                                            rel_eff);
+rtotlist = logspace(2,5,42*4);
+kdlist = logspace(-2,5,62*4);
+paramlist = combvec(rtotlist,kdlist)';
+rel_eff_FULL = reshape(F(paramlist(:,1),paramlist(:,2)),...
+    length(rtotlist), length(kdlist))';
+
+c = viridis;
+colormap(c);
+% rel_eff_FULL((rel_eff_FULL<10.3) .* (rel_eff_FULL>9.8)==1) = 200;
+imagesc(rtotlist,kdlist,rel_eff_FULL) % full heatmap
+set(gca,'XScale','log','YScale','log','fontsize',26);
+pbaspect([1,2,1])
+colorbar()
+
+%% adding empirical value of real receptors
+hold on
+scatter([32000,1572,593,200,22300],...
+            [25000,5,4,12,235],70,'red','filled',...
+                            'MarkerEdgeColor','k')
+scatter([1000,1000,10000,17000,50000],...
+            [0.013,0.019,0.05,12.5,2],70,'white',...
+                            'filled','MarkerEdgeColor','k')
+hold off
+saveas(gca,"panel_6B.svg")
+end
---
+++ figure_6/run_script.m
@@ -0,0 +1,48 @@
+clear
+rtotlist = logspace(2,5,16);
+kdlist = logspace(1,5,21);
+paramlist = combvec(rtotlist,kdlist)';
+nparam = length(paramlist);
+rel_eff_mat = zeros(nparam,1);
+cellrad = 10;
+default_param = struct('rtot',1000,'kd',40,'receptornoise',0.1);
+for jj = 1:nparam
+    %% setting parameters
+    newparam = default_param;
+    newparam.rtot = paramlist(jj,1);
+    newparam.kd = paramlist(jj,2);
+    rel_eff_mat(jj) = MI_opt("tissue","tissue_300by900",cellrad,...
+        "receptor_params",newparam,"saving_data",true,"plot_env",false,...
+        "index",jj);
+    if mod(jj, 10) == 0
+        disp(jj);
+    end
+end 
+save("kd_rtot_rel_eff");
+
+%% part 2
+clear
+rtotlist = logspace(2,5,7);
+kdlist = logspace(-2,0.5,6);
+paramlist = combvec(rtotlist,kdlist)';
+nparam = length(paramlist);
+rel_eff_mat = zeros(nparam,1);
+cellrad = 10;
+default_param = struct('rtot',1000,'kd',40,'receptornoise',0.1);
+for jj = 1:nparam
+    %% setting parameters
+    newparam = default_param;
+    newparam.rtot = paramlist(jj,1);
+    newparam.kd = paramlist(jj,2);
+    rel_eff_mat(jj) = MI_opt("tissue","tissue_300by900",cellrad,...
+        "receptor_params",newparam,"saving_data",true,"plot_env",false,...
+        "index",jj);
+    if mod(jj, 10) == 0
+        disp(jj);
+    end
+end 
+save("kd_rtot_rel_eff_2");
+
+%% plotting
+fnamelist = ["kd_rtot_rel_eff","kd_rtot_rel_eff_2"];
+make_panel_6B(fnamelist)
